5bd5ac4437bb7c51f5bbe78f631e434f7d5487ec
Oriana Toscano Trillo <otoscanot@fi.uba.ar>
2026-05-09 18:24:34 -0300

    Agrego prueba de TOML y dependencias de toml11

--- server/main.cpp
+++ server/main.cpp
@@ -1,3 +1,18 @@
+#include <iostream>
+#include <map>
+#include <toml.hpp>
 #include "common/foo.h"
+#include "common/toml_test.cpp"
 
-int main() { return foo(6); }
+int main() {
+    try {
+        auto root = toml::parse("../items.toml");
+
+        auto equipables = toml::find<std::map<std::string, Equipable>>(root, "equipable");
+
+    } catch (const std::exception& e) {
+        std::cerr << "Error: " << e.what() << '\n';
+    }
+
+    return foo(6);
+}
