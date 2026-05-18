2015975baee44db53ce77d299afcfe421206bb5e
Nicolas-Olivieri <olivierinik22@gmail.com>
2026-05-17 21:58:50 -0300

    Elimino Mock de ClientGame

--- client/client_game.cpp
+++
@@ -1,9 +0,0 @@
-#include "client_game.h"
-
-#include <string>
-#include <utility>
-
-ClientGame::ClientGame(Socket&& socket, const std::string& username):
-        socket(std::move(socket)), username(username) {}
-
-void ClientGame::run() { socket.close(); }
