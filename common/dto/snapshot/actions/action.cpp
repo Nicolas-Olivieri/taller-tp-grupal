7aeebd0ff168eb5264cd4f545dacde52f9d8f7b7
Nicolas-Olivieri <olivierinik22@gmail.com>
2026-05-24 13:27:29 -0300

    Resuelvo bug del serializado del Despawn

--- common/dto/snapshot/actions/action.cpp
+++ common/dto/snapshot/actions/action.cpp
@@ -1,10 +1,22 @@
 #include "action.h"
 
+#include <stdexcept>
+
 #include "common/protocol/serializer.h"
 
 // TODO: este método debería cambiar su comportamiento dependiendo del
 // ActionType, considerar opciones de implementación
-size_t ActionDTO::message_size() const { return sizeof(action); }
+size_t ActionDTO::message_size() const {
+    const auto base = sizeof(action);
+
+    switch (action) {
+        case ActionType::DESPAWN:
+            return base + despawn.message_size();
+        default:
+            throw std::runtime_error("ActionDTO Descubrió que tiene un tipo de acción desconocido al "
+                                     "calcular su message_size");
+    }
+}
 
 void ActionDTO::accept(Serializer& serializer) const { serializer.serialize(*this); }
 
