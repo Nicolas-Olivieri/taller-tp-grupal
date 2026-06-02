1410384b6d14a65c4ffcca0c71d98bd973a08300
Oriana Toscano Trillo <otoscanot@fi.uba.ar>
2026-06-02 02:48:03 -0300

    Agrega cambios en persistencia no commiteados y asset de npc

--- editor/persistance/map_saver.cpp
+++ editor/persistance/map_saver.cpp
@@ -15,9 +15,11 @@ MapSaver::MapSaver(MapData& data): data(data) {}
  * <width en tiles del mundo uint16_t> <height en tiles del mundo uint16_t>
  *
  * <matriz fila por fila indicando si cada tile es walkable o unwalkable uint8_t>
+ * <cant npcs uint16_t> [<id uint8_t> <origen_x uint16_t> <origen_y uint16_t>] [] ...
  *
  * <cant tiles uint16_t> [<id uint8_t> <origen_x uint16_t> <origen_y uint16_t>] [] ...
  * <cant colliders uint16_t> [<id uint16_t> <origen_x uint16_t> <origen_y uint16_t>] [] ...
+ * <cant npcs uint16_t> [<id uint8_t> <origen_x uint16_t> <origen_y uint16_t>] [] ...
  */
 void MapSaver::save(const QString& user_filename) {
     const QString filename = format_filename(user_filename);
@@ -61,10 +63,18 @@ void MapSaver::store_offset_and_dimensions_data(QDataStream& stream) const {
     const uint16_t world_height = matrix_size.height();
     constexpr uint16_t header = HEADER;
 
+    uint16_t npc_amount = 0;
+    for (auto& placement: data.placements) {
+        if (placement.asset.type == ImageType::NPC) {
+            npc_amount++;
+        }
+    }
+
     const uint16_t server_start =
             sizeof(header) + sizeof(uint16_t) * 2 + sizeof(world_width) + sizeof(world_height);
 
-    const uint16_t server_end = server_start + sizeof(uint8_t) * world_width * world_height;
+    const uint16_t server_end = server_start + sizeof(uint8_t) * world_width * world_height +
+                                sizeof(uint16_t) + (sizeof(uint8_t) + sizeof(uint16_t) * 2) * npc_amount;
 
     stream << header << server_start << server_end << world_width << world_height;
 }
@@ -85,16 +95,35 @@ void MapSaver::store_server_data(QDataStream& stream) const {
             stream << is_walkable;
         }
     }
+
+    uint16_t npc_amount = 0;
+    QByteArray npc_data;
+    QDataStream npc_stream(&npc_data, QIODevice::WriteOnly);
+    for (const auto& placement: data.placements) {
+        const uint16_t origin_x = placement.origin.x() - min_point.x();
+        const uint16_t origin_y = placement.origin.y() - min_point.y();
+
+        if (placement.asset.type == ImageType::NPC) {
+            const uint8_t id = placement.asset.id;
+            npc_stream << id << origin_x << origin_y;
+            npc_amount++;
+        }
+    }
+    stream << npc_amount;
+    stream.writeRawData(npc_data.constData(), npc_data.size());
 }
 
 void MapSaver::store_client_data(QDataStream& stream) const {
     uint16_t tile_amount = 0;
     uint16_t collider_amount = 0;
+    uint16_t npc_amount = 0;
 
     QByteArray tile_data;
     QByteArray collider_data;
+    QByteArray npc_data;
     QDataStream tile_stream(&tile_data, QIODevice::WriteOnly);
     QDataStream collider_stream(&collider_data, QIODevice::WriteOnly);
+    QDataStream npc_stream(&npc_data, QIODevice::WriteOnly);
 
     for (const auto& placement: data.placements) {
         const uint16_t origin_x = placement.origin.x() - min_point.x();
@@ -110,10 +139,17 @@ void MapSaver::store_client_data(QDataStream& stream) const {
             collider_stream << id << origin_x << origin_y;
             collider_amount++;
         }
+        if (placement.asset.type == ImageType::NPC) {
+            const uint8_t id = placement.asset.id;
+            npc_stream << id << origin_x << origin_y;
+            npc_amount++;
+        }
     }
 
     stream << tile_amount;
     stream.writeRawData(tile_data.constData(), tile_data.size());
     stream << collider_amount;
     stream.writeRawData(collider_data.constData(), collider_data.size());
+    stream << npc_amount;
+    stream.writeRawData(npc_data.constData(), npc_data.size());
 }
