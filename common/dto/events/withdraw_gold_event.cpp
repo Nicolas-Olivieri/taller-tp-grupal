0ae85517c651446850f34413c322558546d9549b
bogdan <bskorokhod@fi.uba.ar>
2026-06-05 17:49:40 -0300

    Corrige uso de enum incorrecto
    Reemplaza el uso erróneo de CommandType::DEPOSIT_GOLD por CommandType::WITHDRAW_GOLD en el constructor de WithdrawGoldEventDTO

--- common/dto/events/withdraw_gold_event.cpp
+++ common/dto/events/withdraw_gold_event.cpp
@@ -4,7 +4,7 @@
 
 
 WithdrawGoldEventDTO::WithdrawGoldEventDTO(const uint16_t gold_amount):
-        EventDTO(CommandType::DEPOSIT_GOLD), gold_amount(gold_amount) {}
+        EventDTO(CommandType::WITHDRAW_GOLD), gold_amount(gold_amount) {}
 
 
 size_t WithdrawGoldEventDTO::message_size() const { return EventDTO::message_size() + sizeof(gold_amount); }
