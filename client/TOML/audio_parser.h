1b56a994184ed7203ea83c6a6b042639f1bec53f
bogdan <bskorokhod@fi.uba.ar>
2026-06-09 14:11:29 -0300

    Agrega parser para configuraciones de audio
    Implementa template para recuperar la configuración de música almacenada en un archivo TOML.

---
+++ client/TOML/audio_parser.h
@@ -0,0 +1,56 @@
+#ifndef AUDIO_PARSER_H
+#define AUDIO_PARSER_H
+
+#include <algorithm>
+#include <map>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+#include <toml.hpp>
+
+
+struct SoundConfig {
+    std::vector<std::string> paths;
+    uint8_t volume;  // Rango de 0 a 128
+};
+
+
+enum class MusicTrack { LOGIN, CREATOR, FOREST };
+
+struct MusicConfig {
+    std::map<MusicTrack, SoundConfig> configs;
+};
+
+
+template <>
+struct toml::from<MusicConfig> {
+    static MusicConfig from_toml(const toml::value& v) {
+        // TODO: Considerar moverlo a TomlHelper
+        static const std::unordered_map<std::string, MusicTrack> string_to_music_track({
+                {"login", MusicTrack::LOGIN},
+                {"creator", MusicTrack::CREATOR},
+                {"forest", MusicTrack::FOREST},
+        });
+
+        MusicConfig config;
+        for (const auto& [key, value]: v.as_table()) {
+            MusicTrack music_track = string_to_music_track.at(key);
+
+            auto raw_paths = toml::find<std::vector<std::string>>(value, "paths");
+            const int volume = toml::find<int>(value, "volume");
+
+            std::vector<std::string> full_paths;
+            full_paths.reserve(raw_paths.size());
+            std::ranges::transform(raw_paths, std::back_inserter(full_paths),
+                                   [](const std::string& path) { return std::string(DATA_PATH) + path; });
+
+            config.configs[music_track] = SoundConfig(full_paths, volume);
+        }
+
+        return config;
+    }
+};
+
+
+#endif  // AUDIO_PARSER_H
