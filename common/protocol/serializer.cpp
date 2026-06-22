cee94e45c98ffb90535cf3c206865abed83b9160
bono-neer <bneer@fi.uba.ar>
2026-06-22 20:40:13 -0300

    Saco la password de CredentialsDTO
    No era necesario para el scope del TP

--- common/protocol/serializer.cpp
+++ common/protocol/serializer.cpp
@@ -17,7 +17,6 @@ void Serializer::serialize(const ProtocolMessageDTO& dto) { dto.accept(*this); }
 void Serializer::serialize(const CredentialsDTO& credentials) {
     serialize(static_cast<uint8_t>(Message::CREDENTIALS));
     serialize(credentials.username);
-    serialize(credentials.password);
 }
 
 void Serializer::serialize(const EventDTO& event) {
