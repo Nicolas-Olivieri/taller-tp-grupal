4d631d629bcce9744e681a94dbd49cc193882823
Oriana Toscano Trillo <otoscanot@fi.uba.ar>
2026-06-26 02:24:33 -0300

    Arregla ruta de vite

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -5,7 +5,7 @@ import path from "node:path";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/taller-tp-grupal_fork/",
+  base: "/taller-tp-grupal/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
